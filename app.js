b2a209b5d2e0171af22b533c8ca66e1249cd08a3
brian-tura <b.tura2002@gmail.com>
2025-05-29 15:51:01 +0200

    [UPD] Base files created

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("hello")
+});
+
+app.get("/movies")
+
+app.listen(port, () => {
+    console.log(`Server in ascolto sulla porta ${port}`)
+});
---
+++ data/db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'db_blog'
+});
+
+connection.connect((err) => {
+    if(err) {
+        console.log(err)
+    };
+    console.log('Connected to MySQL')
+});
+
+module.exports = connection;
